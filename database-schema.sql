93251d2064af5f5c534ddfb6a06faac19c4ee476
estellacoding <stelladai1028@gmail.com>
2025-08-20 02:21:48 +0800

    feat: add PostgreSQL session table

--- database-schema.sql
+++ database-schema.sql
@@ -70,15 +70,3 @@ WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE INDEX "IDX_session_expire" ON "session" ("expire");
-
--- Session table for PostgreSQL session store
-CREATE TABLE "session" (
-"sid" varchar NOT NULL COLLATE "default",
-"sess" json NOT NULL,
-"expire" timestamp(6) NOT NULL
-)
-WITH (OIDS=FALSE);
-
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
-CREATE INDEX "IDX_session_expire" ON "session" ("expire");
